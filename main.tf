848d267dbc78e7688f59c714ba2bb4f179faf5e2
Ravula-V <85692778+Ravula-V@users.noreply.github.com>
2023-02-18 15:25:11 +0530

    Create main.tf
    new terraform configuration file main.tf created

---
+++ main.tf
@@ -0,0 +1,25 @@
+
+
+# The configuration for the `remote` backend.
+    terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "icmpcdktest
+"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "iCMP"
+         }
+       }
+     }
+
+     # An example resource that does nothing.
+     resource "aws_instance" "app_server" {
+          ami           = "ami-830c94e3"
+          instance_type = "t2.micro"
+
+          tags = {
+            Name = "ExampleTerraformGITSNOW-Instance"
+           }
+        }
